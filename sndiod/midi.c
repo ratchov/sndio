edb79c93bcd0d26c54044b517bda88f28b310c0b
Alexandre Ratchov <alex@caoua.org>
2013-05-12 06:52:10 +0200

    use -q options order as midi/N order

--- sndiod/midi.c
+++ sndiod/midi.c
@@ -449,7 +449,7 @@ port_exit(void *arg)
 struct port *
 port_new(char *path, unsigned int mode, int hold)
 {
-	struct port *c;
+	struct port *c, **pc;
 
 	c = xmalloc(sizeof(struct port));
 	c->path = path;
@@ -457,8 +457,10 @@ port_new(char *path, unsigned int mode, int hold)
 	c->hold = hold;
 	c->midi = midi_new(&port_midiops, c, mode);
 	midi_portnum++;
-	c->next = port_list;
-	port_list = c;
+	for (pc = &port_list; *pc != NULL; pc = &(*pc)->next)
+		; /* nothing */
+	c->next = NULL;
+	*pc = c;
 	return c;
 }
 
