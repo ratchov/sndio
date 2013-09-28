075b3da90988ae8b6cd627cb3bce6edd1e34d6da
Alexandre Ratchov <alex@caoua.org>
2013-09-28 20:44:50 +0200

    fix condition to check if a midi port is referenced

--- sndiod/midi.c
+++ sndiod/midi.c
@@ -492,7 +492,8 @@ port_unref(struct port *c)
 #endif
 	for (rxmask = 0, i = 0; i < MIDI_NEP; i++)
 		rxmask |= midi_ep[i].txmask;
-	if ((rxmask & ~c->midi->self) == 0 && c->state == PORT_INIT && !c->hold)
+	if ((rxmask & c->midi->self) == 0 && c->midi->txmask == 0 &&
+	    c->state == PORT_INIT && !c->hold)
 		port_drain(c);
 }
 
