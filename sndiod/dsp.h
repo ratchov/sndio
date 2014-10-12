c5606fd3f392e510488b372d346b58c454da06ae
Alexandre Ratchov <alex@caoua.org>
2014-10-12 23:17:27 +0200

    dont rely on >> working on signed ints

--- sndiod/dsp.h
+++ sndiod/dsp.h
@@ -124,7 +124,7 @@ struct conv {
 	int bfirst;			/* bytes to skip at startup */
 	unsigned int bps;		/* bytes per sample */
 	unsigned int shift;		/* shift to get 32bit MSB */
-	int sigbit;			/* sign bits to XOR */
+	int bias;			/* bias of unsigned samples */
 	int bnext;			/* to reach the next byte */
 	int snext;			/* to reach the next sample */
 	int nch;
