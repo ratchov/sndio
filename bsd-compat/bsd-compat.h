a858f81abe9aecf96aab305c9700ec1a18b72e18
Alexandre Ratchov <alex@caoua.org>
2014-11-15 13:53:04 +0100

    remove __packed, leoth(), and htole()

--- bsd-compat/bsd-compat.h
+++ bsd-compat/bsd-compat.h
@@ -1,24 +1,9 @@
-#ifndef __packed
-#define __packed	__attribute__((packed))
-#endif
-
-#ifndef HAVE_LETOH
-#include <byteswap.h>
-#include <endian.h>
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-#define htole16(x) (x)
-#define htole32(x) (x)
-#define letoh16(x) (x)
-#define letoh32(x) (x)
-#else
-#define htole16(x) bswap_16(x)
-#define htole32(x) bswap_32(x)
-#define letoh16(x) bswap_16(x)
-#define letoh32(x) bswap_32(x)
-#endif
-#endif
+#ifndef BSD_COMPAT_H
+#define BSD_COMPAT_H
 
 int issetugid(void);
 size_t strlcpy(char *, const char *, size_t);
 size_t strlcat(char *, const char *, size_t);
 long long strtonum(const char *, long long, long long, const char **);
+
+#endif /* !defined(BSD_COMPAT_H) */
