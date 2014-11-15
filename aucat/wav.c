737ca22c4da14cf88e5b8402eee8f6fd39aba3aa
Alexandre Ratchov <alex@caoua.org>
2014-11-15 13:56:12 +0100

    bump copyright year

--- aucat/wav.c
+++ aucat/wav.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Alexandre Ratchov <alex@caoua.org>
+ * Copyright (c) 2008-2014 Alexandre Ratchov <alex@caoua.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
