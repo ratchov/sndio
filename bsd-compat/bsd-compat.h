31b3f1eb9d3f166db9e40cadd47d946fb2783a5d
Alexandre Ratchov <alex@caoua.org>
2014-11-18 10:16:27 +0100

    add comment to warn about function name collisions

--- bsd-compat/bsd-compat.h
+++ bsd-compat/bsd-compat.h
@@ -1,9 +1,14 @@
 #ifndef BSD_COMPAT_H
 #define BSD_COMPAT_H
 
+/*
+ * XXX: these end up exported by libsndio, hide them to avoid possible
+ * collisions with program functions with the same name.
+ */
+
 int issetugid(void);
-size_t strlcpy(char *, const char *, size_t);
 size_t strlcat(char *, const char *, size_t);
+size_t strlcpy(char *, const char *, size_t);
 long long strtonum(const char *, long long, long long, const char **);
 
 #endif /* !defined(BSD_COMPAT_H) */
