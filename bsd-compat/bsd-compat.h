51f6116e120149dc46f2cf8a1ae4b11a2b216656
Alexandre Ratchov <alex@caoua.org>
2015-08-25 15:02:02 +0200

    Hide symbols provided by bsd-compat functions by prefixing them by _sndio_.

--- bsd-compat/bsd-compat.h
+++ bsd-compat/bsd-compat.h
@@ -1,14 +1,24 @@
 #ifndef BSD_COMPAT_H
 #define BSD_COMPAT_H
 
-/*
- * XXX: these end up exported by libsndio, hide them to avoid possible
- * collisions with program functions with the same name.
- */
-
+#ifndef HAVE_ISSETUGID
+#define issetugid _sndio_issetugid
 int issetugid(void);
+#endif
+
+#ifndef HAVE_STRLCAT
+#define strlcat _sndio_strlcat
 size_t strlcat(char *, const char *, size_t);
+#endif
+
+#ifndef HAVE_STRLCPY
+#define strlcpy _sndio_strlcpy
 size_t strlcpy(char *, const char *, size_t);
+#endif
+
+#ifndef HAVE_STRTONUM
+#define strtonum _sndio_strtonum
 long long strtonum(const char *, long long, long long, const char **);
+#endif
 
 #endif /* !defined(BSD_COMPAT_H) */
