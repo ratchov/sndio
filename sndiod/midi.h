cf1ebc72d340a178ea3cc67475b985256f84f3f4
Alexandre Ratchov <alex@caoua.org>
2016-01-09 14:33:10 +0100

    make midi->tickets signed (sign is needed for arithmetics). Found by David Coppa. Thanks.

--- sndiod/midi.h
+++ sndiod/midi.h
@@ -73,7 +73,7 @@ struct midi {
 	unsigned int len;		/* expected ``msg'' length */
 	unsigned int txmask;		/* list of ep we send to */
 	unsigned int self;		/* equal (1 << index) */
-	unsigned int tickets;		/* max bytes we can process */
+	int tickets;			/* max bytes we can process */
 	struct abuf obuf;		/* output buffer */
 };
 
