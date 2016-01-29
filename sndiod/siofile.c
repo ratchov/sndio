96e51185e01136f7400d1569f06477b7168b2a91
Alexandre Ratchov <alex@caoua.org>
2016-01-29 12:12:03 +0100

    warn about blocking at log level 3

--- sndiod/siofile.c
+++ sndiod/siofile.c
@@ -353,7 +353,8 @@ dev_sio_run(void *arg)
 			n = sio_read(d->sio.hdl, data, d->sio.todo);
 			d->sio.todo -= n;
 #ifdef DEBUG
-			if (n == 0 && data == base && !sio_eof(d->sio.hdl)) {
+			if (log_level >= 3 &&
+			    n == 0 && data == base && !sio_eof(d->sio.hdl)) {
 				dev_log(d);
 				log_puts(": read blocked at cycle start\n");
 			}
@@ -436,7 +437,8 @@ dev_sio_run(void *arg)
 			n = sio_write(d->sio.hdl, data, d->sio.todo);
 			d->sio.todo -= n;
 #ifdef DEBUG
-			if (n == 0 && data == base && !sio_eof(d->sio.hdl)) {
+			if (log_level >= 3 &&
+			    n == 0 && data == base && !sio_eof(d->sio.hdl)) {
 				dev_log(d);
 				log_puts(": write blocked at cycle start\n");
 			}
