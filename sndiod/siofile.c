fd4ff877d42b00edff2e773298eddc0624ca69d4
Alexandre Ratchov <alex@caoua.org>
2016-05-25 12:17:48 +0200

    Don't log blocked read or write.

--- sndiod/siofile.c
+++ sndiod/siofile.c
@@ -353,11 +353,6 @@ dev_sio_run(void *arg)
 			n = sio_read(d->sio.hdl, data, d->sio.todo);
 			d->sio.todo -= n;
 #ifdef DEBUG
-			if (log_level >= 3 &&
-			    n == 0 && data == base && !sio_eof(d->sio.hdl)) {
-				dev_log(d);
-				log_puts(": read blocked at cycle start\n");
-			}
 			if (log_level >= 4) {
 				dev_log(d);
 				log_puts(": read ");
@@ -437,11 +432,6 @@ dev_sio_run(void *arg)
 			n = sio_write(d->sio.hdl, data, d->sio.todo);
 			d->sio.todo -= n;
 #ifdef DEBUG
-			if (log_level >= 3 &&
-			    n == 0 && data == base && !sio_eof(d->sio.hdl)) {
-				dev_log(d);
-				log_puts(": write blocked at cycle start\n");
-			}
 			if (log_level >= 4) {
 				dev_log(d);
 				log_puts(": wrote ");
