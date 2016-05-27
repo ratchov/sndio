4772005ac1b930cdfaa228652b0bc02463dd1e03
Alexandre Ratchov <alex@caoua.org>
2016-05-27 17:36:53 +0200

    Give the resamp_do() the exact number input and output samples and provide routines to calculate them. This way we don't rely on it anymore to calculate the bytes procuded/consumed. No behaviour change.

--- aucat/dsp.h
+++ aucat/dsp.h
@@ -147,7 +147,9 @@ int aparams_strtoenc(struct aparams *, char *);
 int aparams_enctostr(struct aparams *, char *);
 int aparams_native(struct aparams *);
 
-void resamp_do(struct resamp *, adata_t *, adata_t *, int *, int *);
+int resamp_ocnt(struct resamp *, int);
+int resamp_icnt(struct resamp *, int);
+void resamp_do(struct resamp *, adata_t *, adata_t *, int, int);
 void resamp_init(struct resamp *, unsigned int, unsigned int, int);
 void enc_do(struct conv *, unsigned char *, unsigned char *, int);
 void enc_sil_do(struct conv *, unsigned char *, int);
