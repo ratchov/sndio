02c2d1521cde8e97d9d45894eedd154226f61402
Alexandre Ratchov <alex@caoua.org>
2016-11-03 08:04:47 +0100

    Add support for OSS/FreeBSD. From Tobias Kortkamp <t@tobik.me>.

--- libsndio/sio_priv.h
+++ libsndio/sio_priv.h
@@ -69,6 +69,9 @@ struct sio_hdl *_sio_aucat_open(const char *, unsigned, int);
 #ifdef USE_SUN
 struct sio_hdl *_sio_sun_open(const char *, unsigned, int);
 #endif
+#ifdef USE_OSS
+struct sio_hdl *_sio_oss_open(const char *, unsigned, int);
+#endif
 #ifdef USE_ALSA
 struct sio_hdl *_sio_alsa_open(const char *, unsigned, int);
 #endif
