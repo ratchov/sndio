a560f09bb686abe6efecc3d57688f459fb94b89a
Alexandre Ratchov <alex@caoua.org>
2017-01-03 08:20:49 +0100

    sync utils.c to sndiod one

--- aucat/utils.c
+++ aucat/utils.c
@@ -96,7 +96,7 @@ log_putx(unsigned long num)
 }
 
 /*
- * store a unsigned decimal in the log
+ * store an unsigned decimal in the log
  */
 void
 log_putu(unsigned long num)
@@ -140,7 +140,7 @@ panic(void)
 }
 
 /*
- * allocate a (small) abount of memory, and abort if it fails
+ * allocate a (small) amount of memory, and abort if it fails
  */
 void *
 xmalloc(size_t size)
@@ -163,6 +163,12 @@ xmalloc(size_t size)
 void
 xfree(void *p)
 {
+#ifdef DEBUG
+	if (p == NULL) {
+		log_puts("xfree with NULL arg\n");
+		panic();
+	}
+#endif
 	free(p);
 }
 
