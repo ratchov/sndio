d7a55c76eec6a66f857352176088b2d404c1e5a9
Alexandre Ratchov <alex@caoua.org>
2018-09-26 13:49:25 +0200

    No need to include sys/stat.h. From miko@.

--- libsndio/mio.c
+++ libsndio/mio.c
@@ -17,7 +17,6 @@
 
 #include <sys/types.h>
 #include <sys/time.h>
-#include <sys/stat.h>
 
 #include <errno.h>
 #include <fcntl.h>
--- libsndio/mio_rmidi.c
+++ libsndio/mio_rmidi.c
@@ -17,7 +17,6 @@
 
 #ifdef USE_RMIDI
 #include <sys/types.h>
-#include <sys/stat.h>
 
 #include <errno.h>
 #include <fcntl.h>
